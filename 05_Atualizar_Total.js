2a40c4d7d64aea7c450674c4dedf1cd1c9a3e487
Nando1510 <117784292+Nando1510@users.noreply.github.com>
2025-08-10 16:08:55 -0300

    Create 05_Atualizar_Total.js

---
+++ 05_Atualizar_Total.js
@@ -0,0 +1,9 @@
+function atualizarTotal() {    //ARRAY
+  const valor_Total_Produtos = nome_Array.reduce((acumulador, item) => {
+
+                              //   VALOR               PREÇO
+      return   acumulador + (item.nome_Input2 * item.nome_Input3) }, 0);
+      
+  document.getElementById('total-compras').textContent =
+    `Total das compras: R$ ${valor_Total_Produtos.toFixed(2)}`;
+}
