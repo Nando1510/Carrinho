929b30cb088359d6d9f5af692718b72123131a9f
Nando1510 <117784292+Nando1510@users.noreply.github.com>
2025-08-10 16:07:29 -0300

    Create 04_Carregar_Dados.js

---
+++ 04_Carregar_Dados.js
@@ -0,0 +1,32 @@
+function carregarDados() {
+    const corpo = document.getElementById('tbody');
+    corpo.innerHTML = '';
+
+    for (let i = 0; i < nome_Array.length; i++) {
+      const item = nome_Array[i]; // pega o produto atual
+
+      const tr = document.createElement('tr');
+      tr.innerHTML = `
+        <td>${item.id}</td>
+        <td>${item.nome_Input1}</td>
+
+        //CASO SEJA UM PREÇO, POR EXEMPLO
+        <td>${item.nome_Input2}</td>
+
+        //CASO SEJA UMA QUANTIDADE, POR EXEMPLO
+        <td>${item.nome_Input3}</td>
+
+        
+        <td>R$ ${(item.nome_Input2 * item.nome_Input3).toFixed(2)}</td>
+        <td>
+          <span class="icon" onclick="iniciarEdicao(this, ${item.id})">✏️</span>
+          <span class="icon" onclick="excluirRegistro(${item.id})">🗑️</span>
+        </td>
+      `;
+
+      corpo.appendChild(tr);
+    }
+
+    atualizarTotal();
+  
+}
