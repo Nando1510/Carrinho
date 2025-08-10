4e22dd03591321134f7d26c4172d2b4b2676050e
Nando1510 <117784292+Nando1510@users.noreply.github.com>
2025-08-10 16:00:06 -0300

    Create botao_Cancelar_02.js
    Cria o botão cancelar que chama a função limpar_Inputs( ) que limpa todos os inputs do formulário.

---
+++ botao_Cancelar_02.js
@@ -0,0 +1,7 @@
+const botao_Cancelar = document.getElementById('cancelar');
+
+botao_Cancelar.addEventListener('click', limpar_Inputs);
+
+ function limpar_Inputs(){
+      document.getElementById('limpar_Inputs').reset();
+  };
